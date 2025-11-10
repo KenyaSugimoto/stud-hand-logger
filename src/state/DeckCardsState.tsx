215b299ed9bd4cdddac3b50c584f6309977b28e6
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2025-11-10 17:44:49 +0900

    リファクタ

--- src/state/DeckCardsState.tsx
+++ src/state/DeckCardsState.tsx
@@ -1,6 +1,5 @@
 import { create } from "zustand";
-import type { Card } from "../types/Card";
-import { generateAllCards } from "../utils/card";
+import type { Card, Rank, Suit } from "../types/Card";
 
 type DeckCardsStateType = {
 	deckCards: Card[];
@@ -15,3 +14,22 @@ export const useDeckCardsState = create<DeckCardsStateType>()((set) => ({
 	unknownCards: 0,
 	setUnknownCards: (count: number) => set(() => ({ unknownCards: count })),
 }));
+
+const generateAllCards = (): Card[] => {
+	const suits: Suit[] = ["s", "h", "d", "c"];
+	const ranks: Rank[] = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
+	const cards: Card[] = [];
+
+	for (const suit of suits) {
+		for (const rank of ranks) {
+			cards.push({
+				rank,
+				suit,
+				id: `${rank}${suit}`,
+				assignedTo: null,
+			});
+		}
+	}
+
+	return cards;
+};
